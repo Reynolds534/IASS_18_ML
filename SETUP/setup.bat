a9495f328cbdcc2568cea332ff5994d84b8de092
mm-wang <mwang27@gmail.com>
2018-07-11 15:16:20 -0400

    updating downloadwheels to be installed after requests

--- SETUP/setup.bat
+++ SETUP/setup.bat
@@ -1,8 +1,7 @@
-python DownloadWheels.py
 python get-pip.py
-setpythonpath python.exe
 rem set PATH=%PATH%;C:\Python27\Scripts
 pip install requests
+python DownloadWheels.py
 pip install "numpy-1.14.4+mkl-cp27-cp27m-win_amd64.whl"
 pip install "scipy-1.1.0-cp27-cp27m-win_amd64.whl"
 pip install "MySQL_python-1.2.5-cp27-none-win_amd64.whl"
